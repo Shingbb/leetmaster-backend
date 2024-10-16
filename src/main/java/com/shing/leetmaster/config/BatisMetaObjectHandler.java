4cbb8e291e2bbe501e626f9484a616332b01464f
LBC <541014133@qq.com>
2024-10-16 19:03:39 +0800

    feat: 新增BatisMetaObjectHandler类，实现自动填充userId字段功能。

---
+++ src/main/java/com/shing/leetmaster/config/BatisMetaObjectHandler.java
@@ -0,0 +1,46 @@
+package com.shing.leetmaster.config;
+
+import cn.dev33.satoken.stp.StpUtil;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author shing
+ */
+@Slf4j
+@Component
+public class BatisMetaObjectHandler implements MetaObjectHandler {
+
+    /**
+     * 插入填充方法，用于在插入数据前自动填充某些字段
+     * @param metaObject 待插入的元对象，通过该对象进行字段填充
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        // 插入操作开始时记录日志
+        log.info("开始插入填充...");
+        // 检查元对象是否包含userId字段，以确定是否需要填充该字段
+        if (metaObject.hasGetter("userId")) {
+            try {
+                // 尝试填充userId字段，使用当前登录用户的ID
+                this.strictInsertFill(metaObject, "userId", Long.class, Long.valueOf(StpUtil.getLoginId().toString()));
+            } catch (Exception e) {
+                // 如果填充过程中发生异常，记录错误日志
+                log.error("插入填充失败：{}", e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * 更新填充方法，用于在更新数据前自动填充某些字段
+     * @param metaObject 待更新的元对象，通过该对象进行字段填充
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        // 更新操作开始时记录日志
+        log.info("开始更新填充...");
+        // 此处可以添加更新填充的逻辑，本示例中未具体实现
+    }
+}
