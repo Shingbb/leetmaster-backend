435d28f352c540418eb2b21c774c4ef2518236f2
LBC <541014133@qq.com>
2024-10-31 16:29:35 +0800

    refactor(service): 优化权限获取和用户信息获取逻辑
    -完善 getPermissionList 方法，添加模拟权限列表
    - 优化 getUserInfo 方法，增加缓存逻辑- 添加 TODO 注释，指引后续优化方向

--- src/main/java/com/shing/leetmaster/service/impl/StpInterfaceImpl.java
+++ src/main/java/com/shing/leetmaster/service/impl/StpInterfaceImpl.java
@@ -23,19 +23,19 @@ public class StpInterfaceImpl implements StpInterface {
 
     @Resource
     private UserService userService;
+
     /**
-     * 返回一个账号所拥有的权限码集合 
+     * 返回一个账号所拥有的权限码集合
      */
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
-         //TODO 待做
         // 本 list 仅做模拟，实际项目中要根据具体业务逻辑来查询权限
         List<String> list = new ArrayList<>();
         list.add("101");
         list.add("user.add");
         list.add("user.update");
         list.add("user.get");
-         list.add("user.delete");
+        list.add("user.delete");
         list.add("art.*");
         return list;
     }
@@ -55,6 +55,8 @@ public class StpInterfaceImpl implements StpInterface {
         }
         //TODO 后面使用Redis来改造 从数据库查询（追求性能的话可以注释，直接走缓存）
         long userId = currentUser.getId();
+
+        // 缓存中没有，则从数据库获取
         currentUser = userService.getById(userId);
         if (currentUser == null) {
             throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
